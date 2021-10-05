acd9b874511198557d8e0c7341526be2b7236a66
jeison2621 <andres.moyano@misena.edu.co>
2021-10-05 17:40:37 -0500

    subiendo mercado liebre a GitHub

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app     = express()
+const path    = require('path') 
+const port    = 3000
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+
+
+app.get('/',(req,res)=>{
+   let ruta = path.resolve(__dirname,'./views/home.html')
+   console.log(ruta)
+    res.sendFile(ruta)
+})
+
+app.listen(port,()=>{
+    console.log("server arriba, port: "+port)
+})      
